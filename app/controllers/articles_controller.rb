c50978a752bfe4f42666f88464dd1965f4032a3a
Shirren Premaratne <shirren.premaratne@gmail.com>
2014-09-19 22:55:11 +1000

    Added some comments and added a nav bar to the top

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,14 @@
 class ArticlesController < ApplicationController
-
-  before_filter only: [:index, :show] do
-    @articles = { '1' => "Functions are not methods", '2' => "Why did the doc place the mug there?", '3' => "I dont know some weird article" }
-  end
-
+  # Retrieves all the articles and responds to the URL /articles (GET verb)
+  # renders the view /views/articles/index.html.erb by default
   def index
+    @articles = Article.all
   end
 
+  # Retrieves a single article by id and responds to the URL /articles/:id (GET Verb)
+  # renders the view /views/articles/show.html.erb by default
   def show
     id = params[:id]
-    @article = @articles[id.to_s]
-    @name = params[:name]
+    @article = Article.find(id)
   end
 end
--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,8 +1,12 @@
 class AuthorsController < ApplicationController
+  # Retrieves all the authors and responds to the URL /authors (GET verb)
+  # renders the view /views/authors/index.html.erb by default
   def index
     @authors = Author.all
   end
 
+  # Retrieves a single author by id and responds to the URL /authors/:id (GET Verb)
+  # renders the view /views/authors/show.html.erb by default
   def show
     id = params[:id]
     @author = Author.find(id)
